fd9e07bce3f0adcbc8615b2e32b1e693db3834ff
furuya@MBP <takashi.furuya.0@gmail.com>
2018-05-01 21:22:25 +0900

    ShareKakeibosのAPIを追加

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from kakeibo.models import Kakeibos, Usages, Resources
+from kakeibo.models import Kakeibos, Usages, Resources, SharedKakeibos
 import json
 from datetime import date, datetime
 from django.http import HttpResponse
@@ -38,6 +38,55 @@ def kakeibo(request):
             elif kakeibo.way == "共通支出":
                 kakeibo.usage = Usages.objects.get(name="共通支出")
             # save
+            kakeibo.save()
+            memo = "Successfully completed"
+
+        except Exception as e:
+            memo = e
+
+    else:
+        memo = "you should use POST"
+
+    # json
+    data = {"memo": memo, }
+    json_str = json.dumps(data, ensure_ascii=False, indent=2)
+    response = HttpResponse(json_str, content_type='application/json; charset=UTF-8', status=None)
+    return response
+
+
+@csrf_exempt
+def shared(request):
+    # add data to DB
+    if request.method == "POST":
+        val = json.loads(request.body.decode())
+        try:
+
+            kakeibo = SharedKakeibos()
+            kakeibo.date = date.today()
+            kakeibo.fee = (val['金額'])
+            kakeibo.way = val['項目']
+            kakeibo.memo = val['メモ']
+            kakeibo.paid_by = val['支払者']
+            if kakeibo.way == "引き落とし":
+                kakeibo.move_from = Resources.objects.get(name=val['引き落とし対象'])
+                kakeibo.usage = Usages.objects.get(name=val['引き落とし項目'])
+            elif kakeibo.way == "振替":
+                kakeibo.move_from = Resources.objects.get(name=val['From'])
+                kakeibo.move_to = Resources.objects.get(name=val['To'])
+            elif kakeibo.way == "収入":
+                kakeibo.move_to = Resources.objects.get(name=val['振込先'])
+                if val['収入源'] == 'その他':
+                    val['収入源'] = 'その他収入'
+                kakeibo.usage = Usages.objects.get(name=val['収入源'])
+            elif kakeibo.way == "支出（現金）":
+                kakeibo.move_from = Resources.objects.get(name='財布')
+                kakeibo.usage = Usages.objects.get(name=val['支出項目'])
+            elif kakeibo.way == "支出（クレジット）":
+                kakeibo.usage = Usages.objects.get(name=val['支出項目'])
+            elif kakeibo.way == "共通支出":
+                kakeibo.usage = Usages.objects.get(name="共通支出")
+            # save
+
             kakeibo.save()
             memo = "Successfully completed"
 
