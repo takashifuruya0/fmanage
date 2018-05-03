75a42dfb62f48b0694e38f748f9a22c692b259c9
furuya@MBP <takashi.furuya.0@gmail.com>
2018-05-03 18:47:35 +0900

    update_credit 実装

---
+++ command/management/commands/update_credit.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from kakeibo.functions import update_records
+
+# BaseCommandを継承して作成
+class Command(BaseCommand):
+    # python manage.py help count_entryで表示されるメッセージ
+    help = 'Updating credit-record'
+
+    # コマンドライン引数を指定します。(argparseモジュール https://docs.python.org/2.7/library/argparse.html)
+    # 今回はblog_idという名前で取得する。（引数は最低でも1個, int型）
+
+    # コマンドが実行された際に呼ばれるメソッド
+    def handle(self, *args, **options):
+        smsg, emsg = update_records.save_credit_to_sql()
+        if smsg != "":
+            self.stdout.write(self.style.SUCCESS(smsg))
+        else:
+            self.stderr.write(emsg)
