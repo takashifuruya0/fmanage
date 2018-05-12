aebdac538f55d9139d48184721d03c7e92d22ec5
furuya@MBP <takashi.furuya.0@gmail.com>
2018-05-12 10:27:01 +0900

    margeの修正

--- kakeibo/urls.py
+++ kakeibo/urls.py
@@ -42,5 +42,6 @@ urlpatterns = [
     url(r'^fig/pie_shared_year$', views.pie_shared_year, name='pie_shared_year'),
     url(r'^fig/barline_usage/(\d+)$', views.barline_usage, name='barline_usage'),
     url(r'^fig/lines_usage/(\d{4})/(\d{1,2})$', views.lines_usage, name='lines_usage'),
+    url(r'^fig/bars_resource$', views.bars_resource, name='bars_resource'),
 ]
 
