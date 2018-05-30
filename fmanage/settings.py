032a90e5b870492cd025f3bbaca06a87d82b2aa7
takashifuruya0 <takashi.furuya.0@gmail.com>
2018-05-30 14:26:58 +0000

    postgresql導入: migration fileは全削除からの新規作成

--- fmanage/settings.py
+++ fmanage/settings.py
@@ -82,8 +82,14 @@ WSGI_APPLICATION = 'fmanage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'fkmanage',
+        #'USER': os.environ.get("FKMANAGE_DB_USER"),
+        #'PASSWORD': os.environ.get("FKMANAGE_DB_PASSWORD"),
+        'USER': 'fkuser',
+        'PASSWORD': 'Kagaya&101',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 # update db settings
