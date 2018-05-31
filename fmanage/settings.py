920d008b39252cddfe9a612d20514f5b5f49a52c
furuya@MBP <takashi.furuya.0@gmail.com>
2018-05-31 21:36:25 +0900

    環境変数ファイル.envを読み込む機構を追加

--- fmanage/settings.py
+++ fmanage/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
+import environ
+env = environ.Env(DEBUG=(bool, False),)  # set default values and casting
+environ.Env.read_env('.env')  # reading .env file
+env('DEBUG')
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
