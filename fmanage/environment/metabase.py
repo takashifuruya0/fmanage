943449a865f829752da32d708378b4f0912baf8c
tfhand <takashi.furuya.0@gmail.com>
2018-06-23 06:51:39 +0000

    metabaseで実行

---
+++ fmanage/environment/metabase.py
@@ -0,0 +1,20 @@
+from fmanage.settings import *
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "fkmanage",
+        "USER": env('FKMANAGE_DB_USER'),
+        "PASSWORD": env('FKMANAGE_DB_PASSWORD'),
+        "HOST": "10.142.0.2",
+        "POST": "",
+    }
+}
+# update db settings
+db_from_env = dj_database_url.config(conn_max_age=400)
+DATABASES['default'].update(db_from_env)
+
+# Debug
+DEBUG = False
