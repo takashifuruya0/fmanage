8e102bc098411ed80b7f7d389dfc71cc909e6071
furuya <>
2018-08-14 18:11:30 +0900

    apiとモデル作成中

--- asset/models.py
+++ asset/models.py
@@ -12,16 +12,6 @@ class Stocks(models):
         return self.name
 
 
-class HoldingStocks(models):
-    objects = None
-    date = models.DateField()
-    stock = models.ForeignKey(Stocks)
-    num = models.IntegerField()
-
-    def __str__(self):
-        return self.stock.name + "/" + self.num
-
-
 class BuyOrders(models):
     objects = None
     datetime = models.DateTimeField()
@@ -41,19 +31,31 @@ class SellOrders(models):
     num = models.IntegerField(null=False, blank=False)
     price = models.FloatField(null=False, blank=False)
     commission = models.IntegerField()
+    account = models.CharField()
 
     def __str__(self):
         return "S/"+self.datetime+":"+self.stock
 
 
+class HoldingStocks(models):
+    objects = None
+    date = models.DateField()
+    stock = models.ForeignKey(Stocks)
+    num = models.IntegerField()
+    average_price = models.FloatField()
+
+    def __str__(self):
+        return self.stock.name + "/" + self.num
+
+
 class Results(models):
-    buyorder = models.ForeignKey(BuyOrders)
-    sellorder = models.ForeignKey(SellOrders)
+    buy_order = models.ForeignKey(BuyOrders)
+    sell_order = models.ForeignKey(SellOrders)
     holding_time = models.IntegerField()
     result = models.IntegerField()
 
     def __str__(self):
-        return self.buyorder+":"+self.sellorder
+        return self.buy_order + ":" + self.sell_order
 
 
 
