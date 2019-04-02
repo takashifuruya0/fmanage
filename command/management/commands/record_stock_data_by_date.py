8569bd70e66c777437c9c35f936960bb35db5956
furuya <>
2019-04-02 18:30:02 +0900

    投資信託は日次金額を取得しない

--- command/management/commands/record_stock_data_by_date.py
+++ command/management/commands/record_stock_data_by_date.py
@@ -16,26 +16,27 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         stocks = Stocks.objects.all()
         for stock in stocks:
-            # kabuoji3.comから取得
-            data = get_info.kabuoji3(stock.code)
-            if data['status']:
-                # 取得成功時
-                for d in data['data']:
-                    # (date, stock)の組み合わせでデータがなければ追加
-                    if StockDataByDate.objects.filter(stock=stock, date=d[0]).__len__() == 0:
-                        sdbd = StockDataByDate()
-                        sdbd.stock = stock
-                        sdbd.date = d[0]
-                        sdbd.val_start = d[1]
-                        sdbd.val_high = d[2]
-                        sdbd.val_low = d[3]
-                        sdbd.val_end = d[4]
-                        sdbd.turnover = d[5]
-                        sdbd.save()
-                self.stdout.write(self.style.SUCCESS('StockDataByDate of "%s" are updated' % stock.code))
-            else:
-                # 取得失敗時
-                logger.error(data['msg'])
-                self.stderr.write(data['msg'])
+            if len(stock.code) == 4:
+                # kabuoji3.comから取得
+                data = get_info.kabuoji3(stock.code)
+                if data['status']:
+                    # 取得成功時
+                    for d in data['data']:
+                        # (date, stock)の組み合わせでデータがなければ追加
+                        if StockDataByDate.objects.filter(stock=stock, date=d[0]).__len__() == 0:
+                            sdbd = StockDataByDate()
+                            sdbd.stock = stock
+                            sdbd.date = d[0]
+                            sdbd.val_start = d[1]
+                            sdbd.val_high = d[2]
+                            sdbd.val_low = d[3]
+                            sdbd.val_end = d[4]
+                            sdbd.turnover = d[5]
+                            sdbd.save()
+                    self.stdout.write(self.style.SUCCESS('StockDataByDate of "%s" are updated' % stock.code))
+                else:
+                    # 取得失敗時
+                    logger.error(data['msg'])
+                    self.stderr.write(data['msg'])
 
         self.stdout.write(self.style.SUCCESS('StockDataByDate are updated'))
