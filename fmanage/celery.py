83e6fdc02fb8152c296e7fdd6b317dc1e6ba5c38
furuya@MBP <takashi.furuya.0@gmail.com>
2019-04-21 10:53:28 +0900

    環境変数の設定

--- fmanage/celery.py
+++ fmanage/celery.py
@@ -1,9 +1,11 @@
 import os
 from celery import Celery
+from django.conf import settings as django_settings
 
 # set the default Django settings module for the 'celery' program.
+environment = "fmanage.environment.metabase" if django_settings.ENVIRONMENT == "metabase" else "fmanage.settings"
 settings = os.getenv(
-   "DJANGO_SETTINGS_MODULE", "fmanage.settings")
+   "DJANGO_SETTINGS_MODULE", environment)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings)
 
 app = Celery('fmanage')
