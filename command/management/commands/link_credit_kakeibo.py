fa1ba3933bda3074cfefc3eb4d77779c484739f5
furuya@MBP <takashi.furuya.0@gmail.com>
2019-06-10 16:42:00 +0900

    修正

--- command/management/commands/link_credit_kakeibo.py
+++ command/management/commands/link_credit_kakeibo.py
@@ -17,34 +17,47 @@ class Command(BaseCommand):
         credits = Credits.objects.all()
         kcs = Kakeibos.objects.filter(way="支出（クレジット）")
         other = Usages.objects.get(name="その他")
-        counter = [0, 0, 0]
+        counter = [0, 0, 0, 0]
+        msgs = []
         for c in credits:
-            if c.kakeibo:
-                continue
-            check = kcs.filter(fee=c.fee, date__year=c.date.year, date__month=c.date.month)
-            num = check.count()
-            if num == 1:
-                c.kakeibo = check[0]
-                c.save()
-                counter[1] += 1
-            elif num == 0:
-                k = Kakeibos()
-                k.date = c.date
-                k.fee = c.fee
-                k.way = "支出（クレジット）"
-                if c.credit_item:
-                    k.usage = c.credit_item.usage
-                    k.memo = c.credit_item.name
+            try:
+                if c.kakeibo:
+                    continue
+                check = kcs.filter(fee=c.fee, date__year=c.date.year, date__month=c.date.month)
+                num = check.count()
+                if num == 1 and check[0].credits_set.count() == 0:
+                    c.kakeibo = check[0]
+                    c.save()
+                    counter[1] += 1
+                elif num == 0:
+                    k = Kakeibos()
+                    k.date = c.date
+                    k.fee = c.fee
+                    k.way = "支出（クレジット）"
+                    if c.credit_item:
+                        k.usage = c.credit_item.usage
+                        k.memo = c.credit_item.name
+                    else:
+                        k.usage = other
+                    k.save()
+                    c.kakeibo = k
+                    c.save()
+                    counter[0] += 1
                 else:
-                    k.usage = other
-                k.save()
-                c.kakeibo = k
-                c.save()
-                counter[0] += 1
-            else:
-                counter[2] += 1
-                pass
-        smsg = "0:{}, 1:{}, over:{}".format(counter[0], counter[1], counter[2])
+                    for ck in check:
+                        if ck.usage == c.credit_item.usage and ck.credits_set.count() == 0:
+                            c.kakeibo = ck
+                            c.save()
+                            counter[2] += 1
+                            break
+                        else:
+                            msgs.append("{} is not {}".format(c.credit_item.usage, ck.usage))
+                            pass
+
+            except Exception as e:
+                self.stdout.write(e)
+        smsg = "0:{}, 1:{}, matched:{}".format(counter[0], counter[1], counter[2])
         if smsg != "":
             self.stdout.write(self.style.SUCCESS(smsg))
+            self.stdout.write("{}".format(msgs))
             logger.info(smsg)
