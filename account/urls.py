f7c3503340814e474bd673dadeaf1eba3b7cdfb2
furuya@MBP <takashi.furuya.0@gmail.com>
2020-01-12 21:21:11 +0900

    logout機能の実装

--- account/urls.py
+++ account/urls.py
@@ -2,14 +2,11 @@
                                                                                 
 from django.conf.urls import url                                                
 from django.contrib.auth import views as auth_views
-                                                                                
 from . import views    
 
 app_name = 'account'
 urlpatterns = [
 # Login, logout
-    #url(r'^login$', auth_views.login, {'template_name': 'account/login.html'}, name='login'),
-    #url(r'^logout$', auth_views.logout, name='logout'),
     url(r'^login$', auth_views.LoginView.as_view(template_name="account/login.html"), name="login"),
-    url(r'^logout$', auth_views.LogoutView, name='logout'),
+    url(r'^logout$', auth_views.LogoutView.as_view(), name='logout'),
 ]
