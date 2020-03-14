4f9b9c6ed989387a5be15e7139d5d3ab2588cee0
furuya@MBP <takashi.furuya.0@gmail.com>
2020-03-14 22:42:17 +0900

    SBI通知のテストを追加

--- web/tests/test_views_api.py
+++ web/tests/test_views_api.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.urls import reverse
-from web.models import AssetStatus, Stock
+from web.models import AssetStatus, Stock, SBIAlert
 from datetime import date
 import json
 
@@ -19,6 +19,9 @@ class APIViewTest(TestCase):
         )
         self.stock3 = Stock.objects.create(
             code="2931113C", name="test3", market="market3", industry="industry", is_trust=True
+        )
+        self.sbialert = SBIAlert.objects.create(
+            stock=self.stock1, type=2, val=1
         )
 
     def test_get_current_vals(self):
@@ -43,6 +46,29 @@ class APIViewTest(TestCase):
         # url
         url = reverse("web:api_create_order")
         self.assertEqual(url, "/nams/api/create_order/")
+
+    def test_recieve_alert(self):
+        # url
+        url = reverse("web:api_receive_alert")
+        self.assertEqual(url, "/nams/api/receive_alert/")
+        # 事前確認
+        self.assertTrue(1, SBIAlert.objects.filter(stock=self.stock1, type=2).count())
+        # POST
+        postdata = {
+          "message": "2020/03/03 13:22に\r\nアラート条件\r\n前日比1\r\n％以上\r\nに達しました。\r\n日経ダブルインバ\r\n1357 東証\r\n現在値:1,063\r\n現時刻:2020/03/03 13:22\r\n前日比:+11\r\n出来高:61,214,318\r\n始値  :1,022\r\n高値  :1,063\r\n安値  :1,015\r\n売気配:1,063\r\n買気配:1,061\r\n買残  :72,668,214\r\n前週比:-2,111,136\r\n売残  :2,301,459\r\n前週比:-494,396\r\n倍率 :+31.57\r\n\r\n-- \r\n古屋敬士\r\nTel  08054506740\r\nMail takashi.furuya.0@gmail.com\r\n",
+          "code": "1570",
+          "val": 1,
+          "type": 2
+        }
+        response = self.client.post(url, data=json.dumps(postdata), content_type="application/json")
+        data = json.loads(response.content)
+        for key in ("status", "message"):
+            self.assertTrue(key in data.keys())
+        # SBIAlertの確認
+        sbialert = SBIAlert.objects.filter(stock=self.stock1, type=2).first()
+        self.assertFalse(sbialert.is_active)
+        self.assertEqual(sbialert.checked_at.date(), date.today())
+        self.assertEqual(sbialert.message, postdata["message"])
 
 
 
---
+++ web/tests/tests_views_sbi.py
@@ -0,0 +1,56 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from web.functions import selenium_sbi
+from web.models import Stock, SBIAlert
+from datetime import date
+
+
+class SBIViewTest(TestCase):
+    def setUp(self) -> None:
+        self.u = User.objects.create_user('HogeTaro', 'taro@hoge.com', 'password')
+        self.client.force_login(user=self.u)
+        self.s = Stock.objects.create(
+            code=1570,
+            name="test{}".format(1),
+            market="market{}".format(1),
+            industry="industry{}".format(1),
+            is_trust=False
+        )
+
+    def tearDown(self) -> None:
+        pass
+
+    def test_set_alert(self):
+        """stockと値、タイプを指定し、SBI証券のアラートを設定する"""
+        # SBIStockは存在しない
+        self.assertEqual(0, SBIAlert.objects.count())
+        # URL
+        url = reverse("web:set_alert")
+        self.assertEqual(url, "/nams/set_alert")
+        # POST
+        data = {
+            "stock": self.s,  # stockオブジェクト
+            "val": 1570,  # 設定する値 (float)
+            "type": 1,  # 0：現在値（円以上）, 1：現在値（円以下）, 2：前日比（％以上）, 3：前日比（％以下）
+        }
+        res = self.client.post(url, data=data)
+        for key in ("status", "msg"):
+            self.assertTrue(key in res.keys())
+        # SBIStockが作成される
+        self.assertEqual(1, SBIAlert.objects.count())
+        sbialert = SBIAlert.objects.first()
+        # {
+        # "type": IntegerField(choices=OPTIONS), 0~4
+        # "stock": ForeignKey(Stock),
+        # "val": Float,
+        # "created_at": datetime,
+        # "checked_at": datetime
+        # }
+        self.assertEqual(sbialert.type, data['type'])
+        self.assertEqual(sbialert.stock, data['stock'])
+        self.assertEqual(sbialert.val, data['val'])
+        self.assertEqual(sbialert.created_at.date(), date.today())
+        self.assertIsNone(sbialert.checked_at)
+        self.assertTrue(sbialert.is_active)
+
