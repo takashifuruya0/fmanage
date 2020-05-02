f7dfd633f3037254ef2b7330e8fb3b7e15c20cc3
furuya@MBP <takashi.furuya.0@gmail.com>
2020-05-02 11:41:54 +0900

    test用のENVIRONMENTを追加

---
+++ fmanage/environment/metabase_test.py
@@ -0,0 +1,32 @@
+from fmanage.settings import *
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "fkmanage",
+        "USER": env('FKMANAGE_DB_USER'),
+        "PASSWORD": env('FKMANAGE_DB_PASSWORD'),
+        'HOST': "127.0.0.1",
+        "POST": "",
+        "ATOMIC_REQUESTS": True,
+    }
+}
+# update db settings
+db_from_env = dj_database_url.config(conn_max_age=400)
+DATABASES['default'].update(db_from_env)
+
+# Debug
+DEBUG = False
+
+# ENVIRONMENT
+ENVIRONMENT = "metabase"
+
+# LOG
+LOGGING['loggers'] = {
+    'django': {
+        'level': 'DEBUG',
+    },
+}
+
