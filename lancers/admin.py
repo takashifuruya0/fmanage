12839b8338e486eedf6d668b9b90a20676cedc83
Takashi Furuya <takashi.furuya.0@gmail.com>
2021-01-20 13:48:21 +0900

    lancersのadmin関連修正

--- lancers/admin.py
+++ lancers/admin.py
@@ -16,21 +16,25 @@ from lancers.forms import OpportunityWorkForm
 class CategoryResource(resources.ModelResource):
     class Meta:
         model = Category
+        exclude = ("created_at", "created_by", "last_updated_at", "last_updated_by", )
 
 
 class ClientResource(resources.ModelResource):
     class Meta:
         model = Client
+        exclude = ("created_at", "created_by", "last_updated_at", "last_updated_by",)
 
 
 class OpportunityResource(resources.ModelResource):
     class Meta:
         model = Opportunity
+        exclude = ("created_at", "created_by", "last_updated_at", "last_updated_by",)
 
 
 class OpportunityWorkResource(resources.ModelResource):
     class Meta:
         model = OpportunityWork
+        exclude = ("created_at", "created_by", "last_updated_at", "last_updated_by",)
 
 
 # ===========================
@@ -257,6 +261,9 @@ class OpportunityWorkAdmin(ImportExportModelAdmin):
 class CategoryAdmin(ImportExportModelAdmin):
     resource_class = CategoryResource
     search_fields = ("name", )
+    readonly_fields = (
+        "created_by", "created_at", "last_updated_by", "last_updated_at",
+    )
 
 
 # ===========================
