6ff6465b79874ee6d2cb8df981ff84b2676b511d
Takashi Furuya <takashi.furuya.0@gmail.com>
2021-01-22 09:34:29 +0900

    バグ修正

--- lancers/models.py
+++ lancers/models.py
@@ -183,7 +183,7 @@ class OpportunityWork(BaseModel):
     opportunity = models.ForeignKey(
         Opportunity, verbose_name="案件", on_delete=models.CASCADE,
         limit_choices_to={
-            "type__in": ("直接受注", "提案受注", "MENTA"),
+            "type__in": ("直接受注", "提案受注", "MENTA", "自己研鑽"),
         }
     )
     datetime_start = models.DateTimeField(verbose_name="開始時間", null=True, blank=True)
